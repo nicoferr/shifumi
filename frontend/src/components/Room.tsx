a6166a99d6aee11ab14519e1cfcf5604540de002
Nicolas Ferrari <nico.ferr@hotmail.fr>
2025-09-20 12:16:54 +0200

    fix rules display

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -21,7 +21,7 @@ export default function Room() {
 
     // On récupère le style de jeu (standard / lizard-spock) dans le state
     useEffect(() => {
-        if(!vsComputer) {
+        if(vsComputer) {
             setGameStyle(state.gameStyle);
         }
     }, []);
@@ -102,17 +102,17 @@ export default function Room() {
                 </>
             }
             </div>
-            <div className="flex w-full justify-center">
-                <button className="btn btn-blue gap-3" onClick={() => setShowRules(true)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                        <path fillRule="evenodd" d="M4.125 3C3.089 3 2.25 3.84 2.25 4.875V18a3 3 0 0 0 3 3h15a3 3 0 0 1-3-3V4.875C17.25 3.839 16.41 3 15.375 3H4.125ZM12 9.75a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H12Zm-.75-2.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75ZM6 12.75a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5H6Zm-.75 3.75a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5H6a.75.75 0 0 1-.75-.75ZM6 6.75a.75.75 0 0 0-.75.75v3c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-3A.75.75 0 0 0 9 6.75H6Z" clipRule="evenodd" />
-                        <path d="M18.75 6.75h1.875c.621 0 1.125.504 1.125 1.125V18a1.5 1.5 0 0 1-3 0V6.75Z" />
-                    </svg>
-                    Rules
-                </button>
-            </div>
             { startGame || vsComputer ? 
                 <div>
+                    <div className="flex w-full justify-center">
+                        <button className="btn btn-blue gap-3" onClick={() => setShowRules(true)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                <path fillRule="evenodd" d="M4.125 3C3.089 3 2.25 3.84 2.25 4.875V18a3 3 0 0 0 3 3h15a3 3 0 0 1-3-3V4.875C17.25 3.839 16.41 3 15.375 3H4.125ZM12 9.75a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H12Zm-.75-2.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75ZM6 12.75a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5H6Zm-.75 3.75a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5H6a.75.75 0 0 1-.75-.75ZM6 6.75a.75.75 0 0 0-.75.75v3c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-3A.75.75 0 0 0 9 6.75H6Z" clipRule="evenodd" />
+                                <path d="M18.75 6.75h1.875c.621 0 1.125.504 1.125 1.125V18a1.5 1.5 0 0 1-3 0V6.75Z" />
+                            </svg>
+                            Rules
+                        </button>
+                    </div>
                     <GameProvider>
                         <Game roomName={roomName} socket={socket} gameStyle={gameStyle} />
                     </GameProvider>
